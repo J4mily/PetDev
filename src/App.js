eedabb420009a6cf42ae2f7dda8de94bd880a62b
Thiago Bignardi <bignardi.thiago@gmail.com>
2024-04-16 21:21:00 -0300

    styles: atualizações no import da home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Home from './pages/home/Home';
+import Home from './pages/home';
 import Animais from './pages/Animais';
 import Quiz from './pages/Quiz';
 import Sobre from './pages/Sobre';
