ac6dd62a3f285b2d78823d44573ee3504225539c
Thiago Bignardi <bignardi.thiago@gmail.com>
2024-05-09 11:37:14 -0300

    feat: implementação página depoimentos

--- src/pages/depoimentos/Depoimentos.js
+++ src/pages/depoimentos/Depoimentos.js
@@ -1,40 +1,17 @@
-/*import styles from "./Depoimentos.module.css";
-import logoPetlove from "../../assets/petlove.png";
-import card1 from "../../assets/card1.png";
+import styles from "./Depoimentos.module.css";
 
-function Depoimentos() {
+
+export const Depoimentos= () => {
   return (
-    <div className={styles.background}>
-      <div className={styles.container}>
-        <div className={styles.container_titulo}>
-          <h1 className={styles.titulo}>Nossos</h1>
-          <h1 className={styles.titulo}>Clientes</h1>
-        </div>
+    
+    <div className={styles.container}>
+      <h1>NOSSOS<br/>CLIENTES</h1>
 
-        <div className={styles.card}>
-          <div className={styles.card_conteudo}>
-            <img src={card1} alt="depoimentos" />
-          </div>
-        </div>
-
-        <div className={styles.clientes_parceiros}>
-          <ClientesParceiros cliente="PetLove" logo={logoPetlove} />
-          <ClientesParceiros cliente="Cobasi" logo={logoPetlove} />
-          <ClientesParceiros cliente="PetShop" logo={logoPetlove} />
-          <ClientesParceiros cliente="Petz" logo={logoPetlove} />
-        </div>
-      </div>
     </div>
+    
   );
 }
 
-function ClientesParceiros({ cliente, logo }) {
-  return (
-    <div className={styles.container_cliente}>
-      <img className={styles.image_logo} src={logo} alt="Logo do cliente" />
-      <p className={styles.cliente_name}>{cliente}</p>
-    </div>
-  );
-}
 
-export { Depoimentos };*/
+
+export default Depoimentos;
