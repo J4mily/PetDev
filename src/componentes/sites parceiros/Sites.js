4a3e699b17407efd57eac083410c8b89da4fdc87
Thiago Bignardi <bignardi.thiago@gmail.com>
2024-05-10 16:56:05 -0300

    feat: implementação carrossel de imagens

---
+++ src/componentes/sites parceiros/Sites.js
@@ -0,0 +1,10 @@
+function Navegacao(){
+    return(
+        <div>
+            <a href="https://www.petlove.com.br/" 
+        target="_blank" 
+        rel="noopener noreferrer">Visite o PetLove</a>
+        </div>
+    )
+}
+export default Navegacao;
