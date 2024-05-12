9332582a26c457ad5bdae2a13dfc60e2da8402a1
Thiago <bignardi.thiago@gmail.com>
2024-05-12 15:10:01 -0300

    feat: implementação navigation & pagination carrossel depoimentos

--- src/componentes/carrossel/Carrossel.js
+++ src/componentes/carrossel/Carrossel.js
@@ -1,29 +1,29 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { A11y, Navigation, Pagination, Scrollbar } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
-import 'swiper/swiper-bundle.css';
-import { A11y, Navigation, Pagination, Scroolbar } from 'swiper/modules';
+import "swiper/swiper-bundle.css";
 
-export const Carrossel = ({slides}) => {
+
+
+export const Carrossel = ({ slides }) => {
   return (
     <Swiper
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
       spaceBetween={50}
       slidesPerView={1}
-      navigation
-      pagination={{clickable: true }}
+      navigation={true}
+      pagination={{ clickable: true }}
       scroolbar={{ draggable: true }}
-      onSlideChange={() => console.log('slide change')}
+      onSlideChange={() => console.log("slide change")}
       onSwiper={(swiper) => console.log(swiper)}
     >
       {slides.map((slide) => (
         <SwiperSlide key={slide.image}>
-          <img src={slide.image} alt={slide.title}/>
+          <img src={slide.image} alt={slide.title} />
         </SwiperSlide>
       ))}
-      
-      
     </Swiper>
   );
 };
-
 
 export default Carrossel;
