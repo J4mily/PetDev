3e42ca0af653f6b0a3fd94f2133f151b40662ec0
Thiago Bignardi <bignardi.thiago@gmail.com>
2024-05-20 17:35:42 -0300

    feat: atualização imagem home; início da página de sugestões

--- src/pages/sugestoes/Sugestoes.js
+++ src/pages/sugestoes/Sugestoes.js
@@ -1,8 +1,14 @@
 export const Sugestoes = () => {
     return(
         <div>
-            <h1>Sugestões</h1>
-            <p>Conteúdo da página</p>
+            <h1>DEIXE SUA SUGESTÃO</h1>
+          <form>
+            <div>
+                <label htmlFor="name">Nome: </label>
+                <input type="text" name="name" placeholder="ESCREVA AQUI SUA SUGESTÃO" />
+            </div>
+            <input type="enviar" value="ENVIAR" />
+          </form>
         </div>
     )
 }
