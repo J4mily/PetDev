ff7d8ccc09247822c07d505efc20f32594e59e63
Natália Paes <100359964+Natalia-Paes@users.noreply.github.com>
2024-06-05 22:33:57 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ import GoldenRetriever from "./pages/animais/Cachorros/goldenretriever";
 import PastorAlemao from "./pages/animais/Cachorros/pastoralemao";
 import ChowChow from "./pages/animais/Cachorros/chowchow";
 
+
 function App() {
   return (
     <Router>
