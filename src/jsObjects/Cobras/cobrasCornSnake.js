20365e0491b6357b77d49744734d63f8f2e5e51b
Natália Paes <100359964+Natalia-Paes@users.noreply.github.com>
2024-06-07 21:23:53 -0300

    Criação página Gopher

--- src/jsObjects/Cobras/cobrasCornSnake.js
+++ src/jsObjects/Cobras/cobrasCornSnake.js
@@ -1,13 +1,13 @@
 import getImage from "../../utilities/getImageLink";
 
 const CornSnakeObj = {
-    imgIntro: getImage ("https://drive.google.com/file/d/1n7hBR1sVxCHLahzMjcXjsh89LYv7D1tL/view?usp=drive_link"),
+    imgIntro: "/images/pag_corn_snake_intro.png",
     altImgIntro: "Imagem Corn Snake Intro",
-    introTitle: "corn snake",
+    introTitle: "CORN SNAKE",
     //Todos os textos que tiverem quebra de linha devem estar entre ` ` para não dar erro no código
     introText: "A Cobra do Milho (Pantherophis guttatus) possui dezenas de variações de cores, resultado principalmente da seleção artificial (criação em cativeiro), são uma espécie terrestre constritora e não venenosa. É nativa do sudoeste dos EUA, partes do México e das Ilhas Cayman. Quando nascem, medem entre 20 cm a 30 cm, e podem chegar a ter um metro e meio quando adultas.",
     generalInformationText: `Corn Snake é o nome que os colonos europeus resolveram dar para essa espécie de cobras que eles comumente encontravam em suas plantações de grãos. É facilmente encontrada em florestas caducifólias, encostas rochosas e áreas agrícolas, onde predomina o clima temperado, mas elas conseguem tolerar bem o clima tropical. São quase vinte classificações de cor, sendo que a maioria é encontrada na forma clássica, apresentando uma coloração alaranjada com manchas vermelhas contornadas de preto.`,
-    imgYouNeedToKnow: getImage("https://drive.google.com/file/d/1IwecgfTOhx8Fj9s_KiqRkGUMngbfBf3X/view?usp=drive_link"),
+    imgYouNeedToKnow: "/images/pag_corn_snake_info.png",
     altImgYouNeedToKnow: "Imagem Corn Snake Info",
     lifetimeText: `Em média de 15 a 20 anos.`,
     estimatedPrice: `A partir de R$800,00.`,
@@ -21,13 +21,13 @@ const CornSnakeObj = {
     Elas crescem em ambientes com 40%, 50% de umidade. Um lago com água fresca, para que, além de beber, elas possam se refrescar e absorver,  o último, principalmente antes da troca de pele.
     
     É preciso também pelo menos dois esconderijos, um na parte mais quente e outro na parte mais fria do terrário, pois serpentes gostam muito de esconder.`,
-    imgProduct1: getImage ("https://drive.google.com/file/d/1nkMbcHSrQt0__ct8MxDFFXc18F3zTYdD/view?usp=drive_link"),
+    imgProduct1: "/images/neonatosc_corn_snake_alimentacao.png",
     altImgProduct1: "Imagem Neonatos C Camundongos",
     textImageProduct1: `Neonatos C - Camundongos`,
-    imgProduct2: getImage ("https://drive.google.com/file/d/1PJS5Gvo2XpfRdIE2W4DbrUN45hRx-wZo/view?usp=drive_link"),
+    imgProduct2: "/images/neonatosa_corn_snake_alimentacao.png",
     altImgProduct2: "Imagem Neonatos A Camundongos",
     textImageProduct2: `Neonatos A - Camundongos`,
-    imgProduct3: getImage("https://drive.google.com/file/d/1VujKxFOq-_NM-iy6-9zK6YXRuWDv6yXL/view?usp=drive_link"),
+    imgProduct3: "/images/neonatos_d_corn_snake_alimentacao.png",
     altImgProduct3: "Imagem Neonatos D Camundongos",
     textImageProduct3: `Neonatos D - Camundongos`,
     curiosityTittle1: `ORIGEM NOME`,
