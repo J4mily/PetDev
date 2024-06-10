66ea3ac184628e7de419e293922a770e5e318158
Natália Paes <100359964+Natalia-Paes@users.noreply.github.com>
2024-06-10 12:30:16 -0300

    Atualiazação Jiboia

--- src/pages/animais/Cobras/Gopher/gopher.js
+++ src/pages/animais/Cobras/Gopher/gopher.js
@@ -11,16 +11,3 @@ export const Gopher = () =>{
 }
 
 export default Gopher;
-import SpeciePageComponent from "../../../../componentes/speciePageComponent/speciePageComponent";
-import GopherObj from "../../../../jsObjects/Cobras/cobrasGopher.js";
-
-export const Gopher = ( ) =>{
-    return(
-        <div>
-            <SpeciePageComponent
-            specieObject = {GopherObj}/>
-        </div>
-    )
-}
-
-export default Gopher;
