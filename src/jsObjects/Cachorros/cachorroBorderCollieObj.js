dd3ba5abb68ed73fa43e8d5de093962b22715623
Thiago <bignardi.thiago@gmail.com>
2024-06-13 17:43:29 -0300

    feature: substituição de imagens chowchow e border collie

--- src/jsObjects/Cachorros/cachorroBorderCollieObj.js
+++ src/jsObjects/Cachorros/cachorroBorderCollieObj.js
@@ -1,11 +1,11 @@
 const cachorroBorderCollieObj = {
-    imgIntro: "https://lh3.google.com/u/0/d/1yIgGYZiASnjBnpd8-R_m2aqjCs-oObjI=w1920-h911-iv2",
+    imgIntro: "/images/pag_border_coolie_intro.png",
     altImgIntro: "Imagem Border Collie",
     introTitle: "Border Collie",
     //Todos os textos que tiverem quebra de linha devem estar entre ` ` para não dar erro no código
     introText: `Os Border Collies são frequentemente considerados uma das raças mais inteligentes de cães. Eles são conhecidos por sua capacidade de aprender comandos complexos e executar tarefas rapidamente. Mesmo os Border Collies criados como animais de estimação em vez de cães de trabalho frequentemente exibem comportamentos de pastoreio, como tentar agrupar crianças ou outros animais.`,
     generalInformationText: `Os Border Collies têm suas origens nas fronteiras entre a Escócia e a Inglaterra, onde foram criados para o pastoreio de ovelhas. A raça é conhecida por sua habilidade excepcional no trabalho com ovelhas e outros animais.  os Border Collies são cães incrivelmente inteligentes, ativos e dedicados, adequados para proprietários que podem fornecer o estímulo físico e mental de que precisam. Eles são companheiros maravilhosos para pessoas ativas e famílias que estão dispostas a investir tempo e energia em seu treinamento e cuidado.`,
-    imgYouNeedToKnow: "https://lh3.google.com/u/0/d/13xz109qgcO0Mt_yf6igyOiLx2BKdtiwa=w1920-h911-iv2",
+    imgYouNeedToKnow: "/images/pag_border_coolie_info.png",
     altImgYouNeedToKnow: "Cachorro Border Collie info",
     lifetimeText: `Em média de 12 a 15 anos.`,
     estimatedPrice: `Varia de 1.500,00 - 3.500,00 um filhote macho`,
@@ -19,13 +19,13 @@ const cachorroBorderCollieObj = {
     Uma dieta balanceada e de alta qualidade é essencial para a saúde e o bem-estar do seu Border Collie. Consulte o veterinário para determinar as necessidades nutricionais específicas do seu cão, levando em consideração fatores como idade, nível de atividade e condições de saúde.
     
     Os Border Collies são cães extremamente leais e gostam de estar perto de seus donos. Dê a seu Border Collie atenção e interação adequadas, incluindo tempo de qualidade, jogos e atividades juntos para fortalecer o vínculo entre vocês.`,
-    imgProduct1: "https://lh3.google.com/u/0/d/1ieos1-AuOkLsPfQoI2kEdLcR2izpJ65I=w1365-h911-iv2",
+    imgProduct1: "/images/racao_royal_border_collie_alimentacao.png",
     altImgProduct1: "Royal canin sensible",
     textImageProduct1: `Royal canin sensible`,
-    imgProduct2: "https://lh3.google.com/u/0/d/18j1F-TkUCQvdV5acs6QMTcTezzcl9A00=w1365-h911-iv2",
+    imgProduct2: "/images/racao_royal_medium_border_collie_alimentacao.png",
     altImgProduct2: "Royal Canin Medium",
     textImageProduct2: `Royal Canin Medium`,
-    imgProduct3: "https://lh3.google.com/u/0/d/1btodzLH0TY6yixdMBLWSiFItGUmCx0eA=w1365-h911-iv2",
+    imgProduct3: "/images/racao_snack_border.png",
     altImgProduct3: "N&D Medium breeds",
     textImageProduct3: `N&D Medium breeds`,
     curiosityTittle1: `Cuidados com a pelagem`,
